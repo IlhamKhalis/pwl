c563b7322dad62e90d43570d3ab67174e30fde16
= IlhamKhalis <= khalisagatha@gmail.com>
2023-03-31 14:10:02 +0700

    Commit 1

---
+++ database/seeders/UserBaruSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserBaruSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'name' => 'Administrator Baru',
+            'email' => 'admin.baru@admin.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
